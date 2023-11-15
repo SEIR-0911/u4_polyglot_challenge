f53679ae2e19a2c1812e0e6f6c73c72a086a209e
sammymarks <sammy.marks@gmail.com>
2023-11-14 23:00:06 -0600

    Sammy Marks - worked a bit with Jaim, Josh H

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import math
+import decimal
+from decimal import Decimal
+
 # Challenge 1:  add_List
 
 # Prompt:
@@ -16,8 +20,22 @@
 # Solution Goes Here - >
 #-----------------------------------------------
 
+def add_list(*nums):
+    # nums comes in as a tuple with a spread operator. Thanks Josh H!
+    sum = 0
 
+    #https://stackoverflow.com/questions/152580/whats-the-canonical-way-to-check-for-type-in-python
+    #https://stackoverflow.com/questions/33311258/python-check-if-variable-isinstance-of-any-type-in-list
+    #https://www.w3schools.com/python/python_datatypes.asp
 
+    for num in nums:
+        if isinstance(num, (int, float, complex)):
+            sum += num
+        else:
+            return "NaN"
+    return sum
+# print(add_list(1,2,3,4,5,6))
+# print(add_list(1,2,3,"a",5,6))
 
 # Challenge 2: remove_ends
 
@@ -33,7 +51,14 @@
 # Solution Goes Here - >
 #-----------------------------------------------
 
+# https://stackoverflow.com/questions/38066836/python-best-way-to-remove-char-from-string-by-index
+# https://www.digitalocean.com/community/tutorials/python-slice-string
 
+def remove_ends(str):
+    # print(str[::-1])
+    return(str[1:-1:1])
+    # return(str[1:(len(str)-1):1])
+# print(remove_ends("abcdefg"))
 
 # Challenge 3: is_palindrome
 
@@ -51,7 +76,34 @@
 #-----------------------------------------------
 # Solution Goes Here - >
 #-----------------------------------------------
+#https://stackoverflow.com/questions/8270092/remove-all-whitespace-in-a-string
+#https://www.datacamp.com/tutorial/case-conversion-python
+# for i in range(2,10):
+#     print(i)
 
+# test_str = "0123456789"
+# print(test_str[0])
+# print(test_str[-0])
+# print(test_str[1])
+# print(test_str[-1])
+
+def is_palindrome(str):
+    str_updated = str.replace(" ","").lower()
+    # print(str_updated)
+    # print (math.ceil(len(str_updated)/2))
+    for i in range(math.ceil(len(str_updated)/2)):
+        # print(f"i = {i}")
+        # print(str_updated[i])
+        # print(str_updated[-i-1])
+        if str_updated[i] != str_updated[-i-1]:
+            return "false"
+    return "true"
+
+    return(str_updated)
+# print(is_palindrome('SEI Rocks'))
+# print(is_palindrome('rotor'))
+# print(is_palindrome('A nut for a jar of tuna'))
+# print(is_palindrome(''))
 
 
 # Challenge 4: is_prime
@@ -69,6 +121,23 @@
 #-----------------------------------------------
 # Solution goes here ->
 #-----------------------------------------------
+
+def is_prime(integer):
+    if integer > 1:
+        for i in range(2,integer): #start at 2, go to one less than integer
+            # print(i, integer%i)
+            if integer%i == 0:
+                return "false"
+        return "true"
+    else:
+        return "invalid entry, number must be greater than 1"
+
+print(is_prime(1))
+print(is_prime(2))
+print(is_prime(3))
+print(is_prime(4))
+print(is_prime(29))
+print(is_prime(200))
 
 
 
@@ -91,6 +160,30 @@
 #-----------------------------------------------
 # Solution Goes Here ->
 #-----------------------------------------------
+# https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
+# https://chat.openai.com/c/a6543af9-ba5c-44d1-98fd-ecf732bd2cad
+shopping_cart = [ 
+  {"item": "headphones", "price": 25},
+  {"item": "speakers", "price": 40 },
+  {"item": "microphone", "price": 70},
+  {"item": "lamp", "price": 15 },
+  {"item": "tower fan", "price": 35 },
+]
+
+def total_checkout_cost(cart, state):
+    total_cost=0
+    for item in shopping_cart:
+        total_cost += round(item["price"]*1.085, 2)
+    if state == "HI" or state == "AK" or state=="TX" or state=="FL":
+        total_cost += 10
+    if state == "AL" or state == "MS" or state=="NV" or state=="IL":
+        total_cost += 5
+    return total_cost
+print(total_checkout_cost(shopping_cart, "IL"))
+print(total_checkout_cost(shopping_cart, "TX"))
+print(total_checkout_cost(shopping_cart, "VA"))
+
+
 
 
 # Challenge 6: fizz_buzz
@@ -108,8 +201,17 @@
 #-----------------------------------------------
 # Solution Goes Here ->
 #-----------------------------------------------
-
-
+def fizz_buzz(i):
+    if not isinstance(i, (int, float, complex)): print(f"{i} is not a Number")
+    elif i%3==0 and i%5==0 : print("FizzBuzz")
+    elif i%3==0 : print("Fizz")
+    elif i%5==0 : print("Buzz")
+    else: print(i)
+fizz_buzz(10)
+fizz_buzz(30)
+fizz_buzz(18) 
+fizz_buzz(22) 
+fizz_buzz("ham_sandwich") 
 
 
 # Challenge 7 - Chessboard Creator
@@ -147,3 +249,26 @@
 #-----------------------------------------------
 # Solution Goes Here - >
 #-----------------------------------------------
+
+def chess_board(row,col):
+    
+    start_O = []
+    for space in range (col):
+        if space%2==0 : start_O.append("O")
+        else : start_O.append("X")
+        space += 1
+
+    start_X = []
+    for space in range (col):
+        if space%2==0 : start_X.append("X")
+        else : start_X.append("O")
+        space += 1
+
+    result = []
+    for r in range(row):
+        if r%2==0: result.append(start_O)
+        else: result.append(start_X)
+    print(result)
+        
+chess_board(3,7)
+chess_board(6,4)
